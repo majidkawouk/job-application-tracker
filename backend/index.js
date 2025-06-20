248962224820ce844e5d5978ec02d99aa4a4ed01
majid <majidkawouk@gmail.com>
2025-06-20 15:41:14 +0300

    add some routes to backend

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,30 @@
-const express = require('express');
-const mysql = require('mysql2');
+const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 
-
 const con = mysql.createPool({
   host: "localhost",
-  user: "root",   
-  password: "",    
+  user: "root",
+  password: "",
   database: "tracker",
-  port: 3306          
+  port: 3306,
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   con.query("SELECT * FROM users", (err, results) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('Database error');
+      return res.status(500).send("Database error");
     }
 
     res.send(results[3]);
   });
 });
 // Route to register user
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { email, password, full_name } = req.body;
   con.query(
     "INSERT INTO users (email, password, full_name) VALUES (?, ?, ?)",
@@ -33,11 +32,11 @@ app.post('/register', (req, res) => {
     (err, results) => {
       if (err) {
         console.error(err);
-        return res.status(500).send('Database error');
+        return res.status(500).send("Database error");
       }
-      res.status(200).send('User added successfully');
+      res.status(200).send("User added successfully");
     }
-  );  
+  );
 });
 // route to user login
 app.post("/login", (req, res) => {
@@ -55,22 +54,55 @@ app.post("/login", (req, res) => {
       }
 
       if (result.length === 0) {
-        return res.status(401).json({ message: "Invalid username or password" });
+        return res
+          .status(401)
+          .json({ message: "Invalid username or password" });
       }
 
       return res.status(200).json({
-  message: "Login successful",
-  user: {
-    id: result[0].id,
-    full_name: result[0].full_name,
-    email: result[0].email
-  }
-});
+        message: "Login successful",
+        user: {
+          id: result[0].id,
+          full_name: result[0].full_name,
+          email: result[0].email,
+        },
+      });
     }
   );
 });
 
+//route to show company
 
+app.post("/dashboard", (req, res) => {
+  const { user_id } = req.body;
+
+  if (!user_id) {
+    return res.status(400).json({ err: "Missing user_id in request body" });
+  }
+
+  con.query(
+    `SELECT * 
+     FROM users u 
+     INNER JOIN applications a ON a.user_id = u.user_id 
+     INNER JOIN companies c ON a.company_id = c.company_id 
+     WHERE u.user_id = ?`,
+    [user_id],
+    (err, result) => {
+      if (err) {
+        console.error("SQL Error:", err);
+        return res.status(500).json({ err: "Database error" });
+      }
+
+      if (result.length === 0) {
+        return res.status(401).json({ err: "Invalid user_id" });
+      }
+
+      return res.json(result);
+    }
+  );
+});
+
+//port
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
